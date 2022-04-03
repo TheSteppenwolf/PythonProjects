fcfb5a25834d8efd0998829d748fc74783344866
Steppenwolf <37122517+TheSteppenwolf@users.noreply.github.com>
2022-04-03 07:36:18 -0500

    Update main.py

--- TreasureIsland/main.py
+++ TreasureIsland/main.py
@@ -3,7 +3,7 @@ Minigame done by: Sebastián Tamayo
 Language: Python
 Date: 02/04/2022
 Description:
-  A simple minigame adventura of a player searching for a treasure in an island full of monsters, ghosts and the mistical treasure. 
+  A simple minigame adventure of a player searching for a treasure in an island full of monsters, ghosts and the mistical treasure. 
   The code was made as part of the course: 100 Days of Code: The Complete Python Pro Bootcamp for 2022
 '''
 
